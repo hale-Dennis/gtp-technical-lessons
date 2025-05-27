9d00e56fe35c80ec24d53def49f3d61d32f34139
Dennis Owusu <haledennis0@gmail.com>
2025-05-27 16:38:45 +0000

    initial commit

---
+++ Spring-MVC-and-Rest/src/main/java/com/demo/Spring/MVC/and/Rest/SpringMvcAndRestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Spring.MVC.and.Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAndRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAndRestApplication.class, args);
+	}
+
+}
---
+++ Spring-MVC-and-Rest/src/test/java/com/demo/Spring/MVC/and/Rest/SpringMvcAndRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Spring.MVC.and.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAndRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
