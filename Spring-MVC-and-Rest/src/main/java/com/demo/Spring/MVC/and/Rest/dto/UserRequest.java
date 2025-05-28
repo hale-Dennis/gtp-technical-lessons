6370a46777bfece094eb42874aee00b6da220512
Dennis Owusu <haledennis0@gmail.com>
2025-05-28 11:14:16 +0000

    refactor: Add a user request dto for better code organization

---
+++ Spring-MVC-and-Rest/src/main/java/com/demo/Spring/MVC/and/Rest/dto/UserRequest.java
@@ -0,0 +1,31 @@
+package com.demo.Spring.MVC.and.Rest.dto;
+
+import com.demo.Spring.MVC.and.Rest.model.User;
+
+public class UserRequest {
+    private String name;
+    private  String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public static UserRequest fromUser(User user) {
+        UserRequest request = new UserRequest();
+        request.setName(user.getName());
+        request.setEmail(user.getEmail());
+        return request;
+    }
+}
