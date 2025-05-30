39696cdd523bd95f45e275009d78f7f52ba5a81c
Dennis Owusu <haledennis0@gmail.com>
2025-05-30 14:51:38 +0000

    feat: Add endpoint for getting paginated cashcards

--- cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
@@ -1,9 +1,10 @@
 package com.example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
