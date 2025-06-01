9e97f5a4319f019dfd244af439066f47d578c9ec
Dennis Owusu <haledennis0@gmail.com>
2025-06-01 18:07:12 +0000

    feat: Add methods in repository to work with spring security

--- cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
@@ -1,5 +1,7 @@
 package com.example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
 }
